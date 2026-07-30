17ab479a1737af8d68452a94fdfebfc50b3ebe43
Chahat Kushwaha <chahatsingh1466@gmail.com>
2026-07-30 17:43:48 +0530

    docs: improve problem description

--- 02-Arrays/Easy-Problems/07_ConcatenationOfArrayLC1929.java
+++ 02-Arrays/Easy-Problems/07_ConcatenationOfArrayLC1929.java
@@ -1,12 +1,8 @@
 /*
  * Problem:
- * Given an integer array `nums` of length `n`,
- * create an array `ans` of length `2n` where:
- *
- * - ans[i] = nums[i]
- * - ans[i + n] = nums[i]
- *
- * Return the array `ans`.
+ * Given an integer array `nums`,
+ * return an array consisting of `nums`
+ * concatenated with itself.
  *
  * Example:
  * Input: nums = [1,2,1]
@@ -15,13 +11,12 @@
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
-// Returns the concatenation of the given array with itself
 public int[] getConcatenation(int[] nums) {
 
     int n = nums.length;
     int[] ans = new int[2 * n];
 
-    // Copy the array twice
+    // Traverse the array
     for (int i = 0; i < n; i++) {
         ans[i] = nums[i];
         ans[i + n] = nums[i];
